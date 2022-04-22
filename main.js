484b9d469813c26dedbb09d3c0c1e11d75544ff8
Jen <jenniferlynnebarry@gmail.com>
2022-04-22 16:48:42 -0700

    passed all tests

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
-// write your code here
-
+    $.getJSON("./data.json", function(data) {
+        var topSpotsData = '';
+   
+    // interate through the data array 
+    // create a table row for each spot 
+    // create a link to the location using long and lat 
+   
+    $.each(data, function(key,value) {
+        topSpotsData += '<tr>';
+        topSpotsData += '<td>' +value.name+'</td>';
+        topSpotsData += '<td>' +value.description+'</td>';
+        topSpotsData += '<td> <a href="https://www.google.com/maps?q=' +value.location+ '">' + "open in google maps" + '</a> </td>';
+        topSpotsData += '</tr>';
+    });
+    $('#top-spots-table').append(topSpotsData);
+});
 });
